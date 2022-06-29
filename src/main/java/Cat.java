fc836424da96d652eb520c445d47f798370afd24
n0rdist <ishevyakin@gmail.com>
2022-06-29 15:50:49 +0400

    1

---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+public class Cat {
+    public Cat() {
+    }
+
+    private Cat cat;
+
+    public Cat getCat() {
+        return cat;
+    }
+
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
+}
